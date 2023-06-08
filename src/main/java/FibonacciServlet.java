a746000225c60388d50bfee5d04b58145b2efb32
Khoa <58025810+khoavnpsc@users.noreply.github.com>
2023-06-08 12:05:18 +1000

    Update

--- src/main/java/FibonacciServlet.java
+++ src/main/java/FibonacciServlet.java
@@ -6,14 +6,26 @@ import javax.servlet.http.HttpServletResponse;
 
 public class FibonacciServlet extends HttpServlet {
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String positionStr = request.getParameter("n");
 
-        int n = Integer.parseInt(request.getParameter("n"));
-        int result = calculateFibonacci(n);
+        try {
+            int n = Integer.parseInt(positionStr);
+            int result = calculateFibonacci(n);
 
-        response.setContentType("text/plain");
-        response.getWriter().write("The Fibonacci number at position " + n + " is: " + result);
+            response.setContentType("text/plain");
+            response.getWriter().write("The Fibonacci number at position " + n + " is: " + result);
+        } catch (NumberFormatException e) {
+            // Handle NumberFormatException
+            response.setContentType("text/plain");
+            response.getWriter().write("Invalid input. Please provide a valid integer value for 'n'.");
+        } catch (IOException e) {
+            // Handle IOException
+            // Log the exception or show an appropriate error message
+            e.printStackTrace();
+            response.setContentType("text/plain");
+            response.getWriter().write("An error occurred while processing the request.");
+        }
     }
 
     private static int calculateFibonacci(int n) {
